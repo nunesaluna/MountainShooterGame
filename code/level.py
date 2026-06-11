44b12083c8edcd6d49b3afdbad282e6dfb5cefce
Natalia <5438327@alunouninter.com>
2026-06-11 13:53:47 -0300

    Creating Players and Enemies Moving Players and Enemies

--- code/level.py
+++ code/level.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
 import sys
 
 import pygame
@@ -7,7 +8,7 @@ from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.const import COLOR_WHITE, WIN_HEIGHT
+from code.const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.entity_factory import EntityFactory
 from code.entity import Entity
 
@@ -19,7 +20,12 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg')) #chamando o metodo get.entity da classe EntityFactory
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
         self.timeout = 20000 #20 segundos
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+
 
     def run(self):
         pygame.mixer.music.load(f'./asset/{self.name}.mp3')
@@ -34,6 +40,9 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit() #fechar a janela no nível 1 = quit
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2')) #aleatoriamente escolhe entre spawns de inimigos 1 e 2
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
             #printed text
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10,5)) #tempo duração da fase
--- code/player.py
+++ code/player.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
 
-from Entity import Entity
+from code.const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_RIGHT, \
+    PLAYER_KEY_LEFT
+from code.entity import Entity
 
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super(). __init__(name, position)
 
     def move(self, ):
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0: #subir a nave somente até o topo
+            self.rect.centery -= ENTITY_SPEED[self.name] #mexendo no eixo y o "retangulo" player
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT: #descer a nave
+            self.rect.centery += ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:  # andar com a nave somente até a borda esquerda
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH: #com elIF, duas teclas ñ poderiam ser pressionadas ao mesmo tempo.
+            self.rect.centerx += ENTITY_SPEED[self.name]
+
         pass
